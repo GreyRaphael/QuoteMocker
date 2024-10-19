37c4113d4e850a08c71bdeb693e4b6bff51a9311
ali_gewei <grey@pku.edu.cn>
2024-10-19 20:49:00 +0800

    finish simple server

--- server.cpp
+++ server.cpp
@@ -1,19 +1,20 @@
+#include <thread>
+
 #include "mocker/publishers.h"
 
 int main(int argc, char const *argv[]) {
-    if (argc < 3) {
-        printf("usage: %s [address] [tick|bar]\n", argv[0]);
+    if (argc < 2) {
+        printf("usage: %s [address]\n", argv[0]);
         return 1;
     }
-    std::string_view cmd{argv[2]};
 
     std::vector<std::string> symbols{"600000", "000001"};
-    if (cmd == "tick") {
-        publish_ticks(argv[1], symbols);
-    } else if (cmd == "bar") {
-        publish_bars(argv[1], symbols);
-    } else {
-        printf("Unknown command:%s\n", argv[2]);
-        return 1;
-    }
+
+    Publisher publisher{argv[1]};
+    std::jthread bar_sender([&publisher, &symbols] {
+        publish_bars(publisher, symbols, 3000);
+    });
+    std::jthread tick_sender([&publisher, &symbols] {
+        publish_ticks(publisher, symbols);
+    });
 }
