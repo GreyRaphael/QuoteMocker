d2b881fd31ef3f0c83e4239add16207af21688e8
ali_gewei <grey@pku.edu.cn>
2024-10-20 22:53:22 +0800

    add recv callback

--- examples/pair1/client.cpp
+++ examples/pair1/client.cpp
@@ -1,6 +1,13 @@
+#include <cstdio>
 #include <mocker/pair1/client.hpp>
+#include <thread>
 
 int main(int argc, char const *argv[]) {
-    Client client{"tcp://127.0.0.1:8889"};
-    client.sub(0, 0, "600000");
+    Client client1{"tcp://127.0.0.1:8889"};
+    client1.sub(0, 0, "600000");
+
+    Client client2{"tcp://127.0.0.1:8889"};
+    client2.sub(0, 0, "600000");
+
+    getchar();
 }
--- mocker/pair1/client.hpp
+++ mocker/pair1/client.hpp
@@ -3,6 +3,7 @@
 #include <flatbuffers/flatbuffer_builder.h>
 #include <nng/nng.h>
 #include <nng/protocol/pair1/pair.h>
+#include <nng/supplemental/util/platform.h>
 
 #include <cstdint>
 
@@ -14,7 +15,7 @@ struct Client {
         nng_dial(socket_, url, NULL, 0);
 
         nng_aio_alloc(&send_aio_, NULL, NULL);
-        nng_aio_alloc(&recv_aio_, NULL, NULL);
+        nng_aio_alloc(&recv_aio_, recv_callback, this);
 
         // set logger
         nng_log_set_logger(nng_stderr_logger);
@@ -32,12 +33,19 @@ struct Client {
         auto topic = Messages::CreateTopicDirect(builder_, market, type, symbol);
         auto topic_msg = Messages::CreateMessage(builder_, Messages::Payload::Topic, topic.Union());
         builder_.Finish(topic_msg);
+
         nng_msg* msg{};
         nng_msg_alloc(&msg, 0);
         nng_msg_append(msg, builder_.GetBufferPointer(), builder_.GetSize());
         nng_aio_set_msg(send_aio_, msg);
+        nng_aio_set_timeout(send_aio_, NNG_DURATION_DEFAULT);
+
         nng_send_aio(socket_, send_aio_);
         nng_aio_wait(send_aio_);
+        nng_log_debug("CLIENT", "client send sub successfully");
+
+        // Start receiving messages
+        nng_recv_aio(socket_, recv_aio_);
     }
 
    private:
@@ -45,4 +53,34 @@ struct Client {
     nng_aio* send_aio_{};
     nng_aio* recv_aio_{};
     flatbuffers::FlatBufferBuilder builder_{1024};
+
+   private:
+    static void recv_callback(void* arg) {
+        auto client = static_cast<Client*>(arg);
+        client->on_recv();
+    }
+
+    void on_recv() {
+        auto msg = nng_aio_get_msg(recv_aio_);
+
+        auto msg_data = nng_msg_body(msg);
+        auto msg_size = nng_msg_len(msg);
+
+        // auto ptr = Messages::GetMessage(msg_data);
+        // switch (ptr->payload_type()) {
+        //     case Messages::Payload::Topic: {
+        //         nng_log_debug("CLIENT", "Topic");
+        //         break;
+        //     }
+        //     default: {
+        //         nng_log_debug("CLIENT", "Unknown payload type, len=%d, content=%s", msg_size, (char*)msg_data);
+        //         break;
+        //     }
+        // }
+
+        nng_log_debug("CLIENT", "Unknown payload type, len=%d, content=%s", msg_size, (char*)msg_data);
+        nng_msg_free(msg);
+        nng_msleep(500);
+        nng_recv_aio(socket_, recv_aio_);
+    }
 };
